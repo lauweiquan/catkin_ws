c4210228bb6edbbcb3da25c2e025c141316c5e6a
lauweiquan <weiquan.yukio@gmail.com>
2018-10-18 09:56:30 +0800

    Add files via upload

---
+++ src/elfin.cpp
@@ -0,0 +1,44 @@
+#include <array>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <Eigen/Dense>
+#include <franka/duration.h>
+#include <franka/exception.h>
+#include <franka/model.h>
+#include <franka/robot.h>
+#include "examples_common.h"
+int main(int argc, char** argv) {
+  // Check whether the required arguments were passed
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <robot-hostname>" << std::endl;
+    return -1;
+  }
+  // Compliance parameters
+  const double translational_stiffness{150.0};
+  const double rotational_stiffness{10.0};
+  Eigen::MatrixXd stiffness(6, 6), damping(6, 6);
+  stiffness.setZero();
+  stiffness.topLeftCorner(3, 3) << translational_stiffness * Eigen::MatrixXd::Identity(3, 3);
+  stiffness.bottomRightCorner(3, 3) << rotational_stiffness * Eigen::MatrixXd::Identity(3, 3);
+  damping.setZero();
+  damping.topLeftCorner(3, 3) << 2.0 * sqrt(translational_stiffness) *
+                                     Eigen::MatrixXd::Identity(3, 3);
+  damping.bottomRightCorner(3, 3) << 2.0 * sqrt(rotational_stiffness) *
+                                         Eigen::MatrixXd::Identity(3, 3);
+  try {
+    // connect to robot
+    franka::Robot robot(argv[1]);
+    
+    // load the kinematics and dynamics model
+    franka::Model model = robot.loadModel();
+    franka::RobotState initial_state = robot.readOnce();
+    std::cout<<initial_state<<std::endl;
+
+  
+  } catch (const franka::Exception& ex) {
+    // print exception
+    std::cout << ex.what() << std::endl;
+  }
+  return 0;
+}
